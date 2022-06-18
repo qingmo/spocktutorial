8a08089b311213abbe853e052250d11d63888bdc
chaosmo <chaosmo.zhu@mexc.com>
2022-06-18 17:57:11 +0800

    feat(project): first commit

---
+++ src/test/groovy/org/chaosmo/MyFirstSpec.groovy
@@ -0,0 +1,11 @@
+package org.chaosmo
+
+import spock.lang.*
+
+// Hit 'Run Script' below
+class MyFirstSpec extends Specification {
+    def "let's try this!"() {
+        expect:
+        Math.max(1, 2) == 3
+    }
+}
